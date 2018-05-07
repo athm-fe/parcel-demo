96e44a81ac2871fb1aeff8350a4553c7dcb051a5
jpuncle <jack_zhxk@163.com>
2018-05-07 15:51:14 +0800

    1. update parcel to latest 2. update demo

--- components/topbar/index.js
+++ components/topbar/index.js
@@ -1,12 +1,9 @@
 import './index.scss';
-import image from './logo.png';
 
 function init () {
   console.log('topbar init');
-  console.log('topbar.image', image);
 }
 
 export default {
   init,
-  image,
 };
--- index/js/index.js
+++ index/js/index.js
@@ -1,26 +1,19 @@
 import _ from 'lodash/core';
-import topbar from '../../components/topbar';
-
 import modES6 from './modES6';
 const modCommonJS = require('./modCommonJS');
 
 import 'normalize.css/normalize.css';
+import topbar from '../../components/topbar';
 import '../../components/footer/index.scss';
-import mainStyle from '../css/main.scss';
-import testStyle from '../css/test.css';
-
+import '../css/main.scss';
 import playURL from '../img/play.png';
 
 console.log('_', _);
-
-topbar.init();
-
 console.log('modES6', modES6);
 console.log('modCommonJS', modCommonJS);
 
-console.log(mainStyle);
-console.log(testStyle);
+topbar.init();
 
 const play = document.createElement('div');
 play.innerHTML = `<img src="${playURL}">`;
-document.body.appendChild(play);
+document.querySelector('.main').appendChild(play);
